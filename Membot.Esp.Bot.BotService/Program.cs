fb5f8a950f6b935b36e3b296f8d0532b29164840
Alexandru Ban <alexandru.ban.drmer@gmail.com>
2022-11-05 12:27:07 +0100

    Fixed error

--- Membot.Esp.Bot.BotService/Program.cs
+++ Membot.Esp.Bot.BotService/Program.cs
@@ -1,11 +1,9 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
 using Membot.Esp.Bot.Common.TokenManager;
-using Membot.Esp.Bot.Logic.Test;
 using Membot.Esp.Bot.Model.Bot.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Hosting;
 using Membot.Esp.Bot.BotService;
 using Microsoft.Extensions.Options;
